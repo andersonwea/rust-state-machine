774b2300b1285acb719e057d0620af2b31e88f0c
chain <klaus.mika1052@gmail.com>
2024-11-02 19:22:42 -0300

    create function to trasfer balances

--- src/balances.rs
+++ src/balances.rs
@@ -18,14 +18,56 @@ impl Pallet {
   pub fn get_balance(&self, account: &str) -> u128 {
     *self.balances.get(account).unwrap_or(&0)
   }
+
+  pub fn transfer_balance(
+    &mut self, 
+    from: &str, 
+    to: &str, 
+    amount: u128
+  ) -> Result<(), &'static str> {
+    let from_balance = self.get_balance(&from);
+    let to_balance = self.get_balance(&to);
+
+    let new_from_balance = from_balance
+      .checked_sub(amount)
+      .ok_or("Insufficient balance.")?;
+
+    let new_to_balance = to_balance
+      .checked_add(amount)
+      .ok_or("Overflow.")?;
+
+    self.set_balance(&from, new_from_balance);
+    self.set_balance(&to, new_to_balance);
+
+    Ok(())
+  }
 }
 
-#[test]
-fn init_balances() {
-  let mut balances = Pallet::new();
+mod test {
+    use super::Pallet;
 
-  assert_eq!(balances.get_balance(&"alice"), 0);
-  balances.set_balance(&"alice", 100);
-  assert_eq!(balances.get_balance(&"alice"), 100);
-  assert_eq!(balances.get_balance(&"bob"), 0);
+  #[test]
+  fn init_balances() {
+    let mut balances = Pallet::new();
+  
+    assert_eq!(balances.get_balance(&"alice"), 0);
+    balances.set_balance(&"alice", 100);
+    assert_eq!(balances.get_balance(&"alice"), 100);
+    assert_eq!(balances.get_balance(&"bob"), 0);
+  }
+  
+  #[test]
+  fn transfer_balance() {
+    let mut balances = Pallet::new();
+
+    assert_eq!(balances.transfer_balance(&"alice", &"bob", 5), Err("Insufficient balance."));
+
+    balances.set_balance(&"alice", 10);
+    balances.set_balance(&"bob", 5);
+
+    assert_eq!(balances.transfer_balance(&"alice", &"bob", 5), Ok(()));
+    assert_eq!(balances.get_balance(&"alice"), 5);
+    assert_eq!(balances.get_balance(&"bob"), 10);
+  }
+
 }

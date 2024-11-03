d9fa9b50c94d8f66ccca5c7e2955b21cd5287a22
chain <klaus.mika1052@gmail.com>
2024-11-03 18:15:10 -0300

    implement increment nonce and block number in system pallet

--- src/system.rs
+++ src/system.rs
@@ -12,4 +12,33 @@ impl Pallet {
       nonce: BTreeMap::new(),
     }
   }
+
+  pub fn block_number(&self) -> u32 {
+    self.block_number
+  }
+
+  pub fn inc_block_number(&mut self) {
+    self.block_number += 1
+  }
+
+  pub fn inc_nonce(&mut self, account: &str) {
+    let account_nonce = self.nonce.get(account).unwrap_or(&0);
+
+    self.nonce.insert(account.to_string(), account_nonce + 1);
+  }
+}
+
+mod test {
+    use super::Pallet;
+
+  #[test]
+  fn init_system() {
+    let mut system = Pallet::new();
+
+    system.inc_block_number();
+    system.inc_nonce(&"alice");
+
+    assert_eq!(system.block_number(), 1);
+    assert_eq!(system.nonce.get(&"alice".to_string()), Some(&1));
+  }
 }
